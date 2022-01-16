3c24bf25edd3716ec94d8a3dd5398901179e80cf
yangyiqiang <yangyiqiang@verycd.com>
2022-01-17 07:42:25 +0800

    add

---
+++ my_ml_angents/Assets/Scripts/RollerAgent.cs
@@ -0,0 +1,18 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class RollerAgent: Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
