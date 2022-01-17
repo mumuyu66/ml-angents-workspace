59166bd50a93554e745efe0ec9719fd7045322ca
yangyiqiang <yangyiqiang@xindong.com>
2022-01-17 10:36:53 +0800

    add angents

--- my_ml_angents/Assets/Scripts/RollerAgent.cs
+++ my_ml_angents/Assets/Scripts/RollerAgent.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class RollerAgent: Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rbody;
+    public GameObject target;
+
+    public void Start()
     {
-        
+        rbody = this.gameObject.GetComponent<Rigidbody>();    
+    }
+    public override void OnEpisodeBegin()
+    {
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(0, 0.5f, 0);
+        }
+        transform.position = new Vector3(Random.value * 8 - 4,0.5f,Random.value * 8 - 4);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(target.transform.position);
+        sensor.AddObservation(rbody.velocity.x);
+        sensor.AddObservation(rbody.velocity.z);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        rbody.AddForce(vectorAction[0] * forceMultiplier, 0, vectorAction[1] * forceMultiplier);
+        if (Vector3.Distance(target.transform.position, transform.position) < 1.42f)
+        {
+            AddReward(1.0f);
+            EndEpisode();
+        }
+
+        if (transform.position.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
     }
 }
